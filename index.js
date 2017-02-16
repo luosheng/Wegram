026d58b56c7ef0e8edd4d1377e97d5d06d4754ad
Luo Sheng <luosheng1986@gmail.com>
2017-02-16 22:19:07 +0800

    Echo received messages

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const { Wechaty } = require('wechaty');
+const { Wechaty, Message } = require('wechaty');
 
-Wechaty.instance()
-.on('message', (message) => {
-    console.log(message);
-})
-.init();
+const wechaty = Wechaty.instance();
+
+wechaty.init();
+wechaty.on('message', (message) => {
+    console.log(message.from());
+    const reply = new Message();
+    reply.to(message.from());
+    reply.content(`Message received: ${message}`);
+    wechaty.send(reply);
+});
